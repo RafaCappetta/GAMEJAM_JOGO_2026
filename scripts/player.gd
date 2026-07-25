80d0856543dd8c4146bfc595635546f323c4d4f7
Ian Felipe <ian.felipe.drunen@gmail.com>
2026-07-24 21:35:26 -0300

    Adding punching animation and player's portrait

--- scripts/player.gd
+++ scripts/player.gd
@@ -69,6 +69,9 @@ func _physics_process(delta: float) -> void:
 		tween.tween_property(self, "dash_velocity", 0, 0.3).set_ease(Tween.EASE_OUT)
 		%dash_cooldown.start()
 		can_dash = false
+		
+	if Input.is_action_just_pressed("soco"):
+		%animacao.play("soco")
 
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
